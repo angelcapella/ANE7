8e8d8f4c33d6430ca66a092dbdfef01544a959fd
DESKTOP-G7I7M5O\kape9 <angelcapellaruiz@gmail.com>
2020-12-21 09:00:44 +0100

    poniendo homeview y productview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentProductoComponent } from './component-producto/component-producto.component';
+import { HomeViewComponent } from './home-view/home-view.component';
+import { ProductViewComponent } from './product-view/product-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentProductoComponent
+    ComponentProductoComponent,
+    HomeViewComponent,
+    ProductViewComponent
   ],
   imports: [
     BrowserModule,
